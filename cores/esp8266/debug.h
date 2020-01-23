28f1e3af6f0511ab8c28d4c2fd7716e34f9a4c34
chlous <thomas.klaus@gmx.ch>
2020-01-23 12:44:28 +0100

    debug defines added

--- cores/esp8266/debug.h
+++ cores/esp8266/debug.h
@@ -29,5 +29,28 @@ void __panic_func(const char* file, int line, const char* func) __attribute__((n
 }
 #endif
 
+#ifdef DEBUG_IBRICKS_LEVEL0
+#define RELEASE_PRINTLN(...) Serial.println(__VA_ARGS__)
+#define RELEASE_PRINTF(...) Serial.printf(__VA_ARGS__)
+#else
+#define RELEASE_PRINTLN(...)
+#define RELEASE_PRINTF(...)
+#endif
+
+#ifdef DEBUG_IBRICKS_LEVEL1
+#define DEBUG_LOOP(...) Serial.println(__VA_ARGS__)
+#define DEBUG_ERROR(...) Serial.println(__VA_ARGS__)
+#define DEBUG_INFO(...) Serial.println(__VA_ARGS__)
+#else
+#define DEBUG_LOOP(...)
+#define DEBUG_ERROR(...)
+#define DEBUG_INFO(...)
+#endif
+
+#ifdef DEBUG_IBRICKS_LEVEL2
+#define DEBUG_DEBUG(...) Serial.println(__VA_ARGS__)
+#else
+#define DEBUG_DEBUG(...)
+#endif
 
 #endif//ARD_DEBUG_H
